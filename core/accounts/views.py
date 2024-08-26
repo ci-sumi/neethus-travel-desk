27cefccd35bb819d9a8ef1ed220a75bb666a77af
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-08-26 19:08:19 +0100

    Replace standard UserCreationForm with customized SignupForm

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,'index.html',{'show_services':True})
 
 def register(request):
-    return render(request,'register.html',{'show_services':False})
-
+    if request.method=="POST":
+       form = SignupForm(request.POST)
+       if form.is_valid():
+            form.save()
+            messages.success(request,"Account created successfully")
+            return redirect('login')
+       else:
+           messages.error(request,"Error")
+    else:
+        form = SignupForm()
+        
+    return render(request,'register.html',{'form':form})
 
 def login(request):
     return render(request,'login.html',{'show_services':False})
