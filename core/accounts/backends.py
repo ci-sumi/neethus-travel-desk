43324c74fed7512cf1e63f20745f920a802c4c9f
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-08-27 20:09:25 +0100

    Update custom authentication backend for email or username login

--- core/accounts/backends.py
+++ core/accounts/backends.py
@@ -3,11 +3,20 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 class EmailOrUserName(ModelBackend):
-    def authenticate(self, request, username=None, password=None,**kwargs):
-        UserModel=get_user_model()
-        users=UserModel.objects.filter(Q(username=username)| Q(email=username))
-        for user in users:
-            if user.check_password(password):
-                return user
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        
+        try:
+            # Try to get the user by username or email
+            user = UserModel.objects.get(Q(username=username) | Q(email=username))
+        except UserModel.DoesNotExist:
+            return None
+        except UserModel.MultipleObjectsReturned:
+            # Handle the case where multiple users are returned
+            # This might happen if both username and email are not unique.
+            return None
+        
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
         
         return None
