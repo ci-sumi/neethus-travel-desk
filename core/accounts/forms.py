416cce313dca53e3fe2dd5643b583578078fcd15
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-10 14:32:51 +0100

    Add ContactForm class for handling contact form submissions

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Contact
 
 class SignupForm(UserCreationForm):
     username=forms.CharField(max_length=100)
@@ -37,3 +38,8 @@ class SignupForm(UserCreationForm):
         return password2
         
     
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model=Contact
+        fields="__all__"
