0679fc5b13a1b3d66e778dce3ba6f9245e1fb8bb
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-10 12:36:53 +0100

    Add Contact model with fields

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField(max_length=200)
+    phone_number=models.IntegerField()
+    contact_message=models.TextField()
+    time_stamp = models.DateField(auto_now_add=True,blank=True)
