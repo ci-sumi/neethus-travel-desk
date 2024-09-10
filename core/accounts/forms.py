622d84e3c2e99359ed5989e89465062ef963e140
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-10 15:43:23 +0100

    Customize form field widgets with CSS classes and placeholders

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -43,3 +43,14 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model=Contact
         fields="__all__"
+             
+        widgets ={'name':forms.TextInput(attrs={'class':'form-control',
+                                        'placeholder':'Your name'}),
+          'email':forms.EmailInput(attrs={'class':'form-control',
+                                        'placeholder':'Your email'}),
+          'phone_number': forms.TextInput(attrs={'class': 'form-control',
+                                                 'placeholder': 'Your Phone Number'}),
+            'contact_message': forms.Textarea(attrs={'class': 'form-control',
+                                                     'rows': 5, 'placeholder': 'Your Message'}),}
+        
+   
