e94427dc2757f68dc9f8dd5149e0f3abff02f716
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-11 11:33:49 +0100

    Create DestinationForm with customized widgets for improved user input

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Contact
+from .models import Destination
 
 class SignupForm(UserCreationForm):
     username=forms.CharField(max_length=100)
@@ -51,6 +52,19 @@ class ContactForm(forms.ModelForm):
           'phone_number': forms.TextInput(attrs={'class': 'form-control',
                                                  'placeholder': 'Your Phone Number'}),
             'contact_message': forms.Textarea(attrs={'class': 'form-control',
-                                                     'rows': 5, 'placeholder': 'Your Message'}),}
+                                                     'rows': 5, 
+                                                     'placeholder': 'Your Message'}),}
         
    
+class DestinationForm(forms.ModelForm):
+    class Meta:
+        Model= Destination
+        fields ='__all__'
+        widgets ={'name':forms.TextInput(attrs={'class':'form-control'}),
+                'country':forms.TextInput(attrs={'class':'form-control'}),
+                'description':forms.Textarea(attrs={'class':'form-control'}),
+                'best_time_to_visit':forms.Select(attrs={'class':'form-control'}),
+                'budget_type':forms.Select(attrs={'class':'form-control'}),
+                'image':forms.ClearableFileInput(attrs={'class':'form-control'}),
+                'is_favorite':forms.CheckboxInput(attrs={'class':'form-control'})}
+       
