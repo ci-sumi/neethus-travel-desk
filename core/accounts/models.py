a5f980dc0fdbf5dd9cfb42bb8a3c84e6c90982ce
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-11 11:19:07 +0100

    Add Destination model with key fields and image support

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 class Contact(models.Model):
@@ -7,3 +8,28 @@ class Contact(models.Model):
     phone_number=models.IntegerField()
     contact_message=models.TextField()
     time_stamp = models.DateField(auto_now_add=True,blank=True)
+    
+    
+class Destination(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    SEASON_CHOICES = [('Spring','Spring'),
+                      ('Summer','Summer'),
+                      ('Spring','Spring'),
+                      ('Winter','Winter')]
+    best_time_to_visit = models.CharField(max_length=6,choices=SEASON_CHOICES,
+                                          default='Spring')
+    BUDGET_CHOICES = [('Budget','Budget'),
+                      ('Mid-range','Mid-range'),
+                      ('Luxury','Luxury')]
+    budget_type = models.CharField(max_length=10,
+                                   choices=BUDGET_CHOICES,
+                                   default='Mid-range')
+    image = models.ImageField(upload_to='destinations/',blank=True,null=True)
+    is_favorite = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    
