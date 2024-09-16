04c64ab508c0dca7fc38f86f2df148214b7a29f8
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-16 11:52:59 +0100

    Add URL pattern for destination detail view

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login',views.login,name='login'),
     path('logout',CustomLogoutView.as_view(),name='logout'),
     path('contact',views.contact,name='contact'),
-    path('destinations',views.destination_list,name='destination_list'),
-    path('destinations/new',views.destination_create,name='destination_create')
+    path('destinations_list/',views.destination_list,name='destination_list'),
+    path('destinations/new',views.destination_create,name='destination_create'),
+    path('destination_detail/<int:pk>/',views.destination_detail,name='destination_detail'),
 ] 
