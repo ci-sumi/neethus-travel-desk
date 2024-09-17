61e2e6680c44ccc05d1e5799941fbb22e84c5b03
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-09-17 11:45:40 +0100

    Add root URL to display index page as homepage

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -5,7 +5,7 @@ from .views import CustomLogoutView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('index',views.index,name='index'),
+    path('',views.index,name='index'),
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     path('logout',CustomLogoutView.as_view(),name='logout'),
