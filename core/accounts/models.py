912fe409573f70fb48cd47c20444e2d83053011f
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-10-11 12:56:46 +0100

    Add related_name to UserProfile's user field

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -42,7 +42,7 @@ class Destination(models.Model):
     
     
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='user_profile')
     profile_picture =models.ImageField(upload_to='profile_picture',blank=True,null=True)
     
     
