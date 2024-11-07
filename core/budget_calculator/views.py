f6f6817d050923db925ba42a4b0cb2ecd07a42aa
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-11-07 12:16:56 +0000

    Add model for budget calculation and update forms and views for budget calculation

--- core/budget_calculator/views.py
+++ core/budget_calculator/views.py
@@ -1,27 +1,24 @@
-from django.shortcuts import render
+# In your views.py
+from django.shortcuts import render,redirect
 from .forms import BudgetCalculatorForm
-from accounts.models import Destination
-# Create your views here.
-
+from .models import BudgetCalculator
 
 def budget_calculator(request):
     total_cost = None
-    form = BudgetCalculatorForm()
+
     if request.method == 'POST':
         form = BudgetCalculatorForm(request.POST)
         if form.is_valid():
-            accomadation_cost = form.cleaned_data['accomadation_cost']
-            food_cost = form.cleaned_data['food_cost']
-            transportation_cost = form.cleaned_data['transportation_cost']
-            activity_cost = form.cleaned_data['activity_cost']
-            number_of_adults = form.cleaned_data['number_of_adults']
-            number_of_infants = form.cleaned_data['number_of_infants']
-            destination = form.cleaned_data['destination']
+            budget = form.save(commit=False)
+            total_cost = budget.total_cost()
+            budget.save()
             
             
-            total_food_cost = food_cost * (number_of_adults + number_of_infants)
-            total_cost = (accomadation_cost + transportation_cost + activity_cost+total_food_cost)
             
-    return render(request, 'budget.html',{'form':form, 'total_cost':total_cost})
-            
-        
+            # Pass an empty form instance to reset fields on page render
+            # form = BudgetCalculatorForm()  # Reset form fields
+
+    else:
+        form = BudgetCalculatorForm()
+
+    return render(request, 'budget.html', {'form': form, 'total_cost': total_cost})
