51f661c354605880c1f9f90be626ccabd8925d4a
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-20 09:08:12 +0000

    Modify setting.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,7 +21,9 @@ import cloudinary.api
 from dotenv import load_dotenv
 
 
-load_dotenv(override=True)
+load_dotenv(override=False)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,7 +37,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+# DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 # CLOUDINARY_URL=os.environ.get('CLOUDINARY_URL')
 CLOUDINARY_STORAGE = {
@@ -187,3 +190,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 print(SECRET_KEY)
+print(DEBUG)
